c70533cd5fcb48ac93980740a6dc7c4ece9972a5
vincent <vincent.paturet@entur.org>
2021-06-02 16:02:51 +0200

    Added index for RoutePoints

---
+++ src/test/java/org/entur/netex/TestLineExport.java
@@ -0,0 +1,27 @@
+package org.entur.netex;
+
+import org.entur.netex.index.api.NetexEntitiesIndex;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestLineExport {
+    private static NetexEntitiesIndex index;
+
+    @BeforeAll
+    static void init() {
+        try {
+            var parser = new NetexParser();
+            index = parser.parse("src/test/resources/common_file.zip");
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void testGetRoutePoint() {
+        var routePoint = index.getRoutePointIndex().get("AVI:RoutePoint:76586");
+        Assertions.assertNotNull(routePoint);
+    }
+
+}
