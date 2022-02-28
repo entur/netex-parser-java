0fdaabd288872493919c59f4739236731ed894a3
Tom Erik Støwer <testower@gmail.com>
2022-02-28 13:32:35 +0100

    Cache all latest versions lazily and add method to get them directly

--- src/main/java/org/entur/netex/index/impl/VersionedNetexEntityIndexImpl.java
+++ src/main/java/org/entur/netex/index/impl/VersionedNetexEntityIndexImpl.java
@@ -5,9 +5,11 @@ import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import org.entur.netex.index.api.VersionedNetexEntityIndex;
 import org.rutebanken.netex.model.EntityInVersionStructure;
+import org.rutebanken.netex.model.EntityStructure;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.entur.netex.support.NetexVersionHelper.latestVersionedElementIn;
 import static org.entur.netex.support.NetexVersionHelper.versionOfElementIn;
@@ -15,14 +17,27 @@ import static org.entur.netex.support.NetexVersionHelper.versionOfElementIn;
 public class VersionedNetexEntityIndexImpl<V extends EntityInVersionStructure> implements VersionedNetexEntityIndex<V> {
     private final Multimap<String,V> map  = Multimaps.synchronizedListMultimap(ArrayListMultimap.create());
 
+    private Map<String,V> latestMap;
+
     @Override
     public V getLatestVersion(String id) {
-        return latestVersionedElementIn(map.get(id));
+        if (latestMap == null) {
+            populateLatestMap();
+        }
+        return latestMap.get(id);
     }
 
     @Override
     public V getVersion(String id, String version) {
         return versionOfElementIn(map.get(id), version);
+    }
+
+    @Override
+    public Collection<V> getLatestVersions() {
+        if (latestMap == null) {
+            populateLatestMap();
+        }
+        return latestMap.values();
     }
 
     @Override
@@ -44,5 +59,11 @@ public class VersionedNetexEntityIndexImpl<V extends EntityInVersionStructure> i
 
     private void put(V v) {
         map.put(v.getId(), v);
+    }
+
+    private void populateLatestMap() {
+        latestMap = map.keySet().stream()
+                .map(id -> latestVersionedElementIn(map.get(id)))
+                .collect(Collectors.toMap(EntityStructure::getId, e -> e));
     }
 }
